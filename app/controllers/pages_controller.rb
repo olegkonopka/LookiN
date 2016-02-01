4c2acd2b5ada40c21d3313fefa6e31b11301be8a
Konopka Oleg <godschildknopik@gmail.com>
2016-02-01 16:51:23 +0200

    Adding username to URL

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,6 +7,12 @@ class PagesController < ApplicationController
   end
 #back end code for pages/profile
   def profile
+  	#grab the username from the URL as :id
+  	if(User.find_by_username(params[:id]))
+  		@username= params[:id]
+  	else
+  		redirect_to root_path, :notice=> "User not found!!!"
+  	end	
   end
 #back end code for pages/explore
   def explore
