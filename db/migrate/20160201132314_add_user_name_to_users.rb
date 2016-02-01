45cf581d9756c6de2a2ef443211cf126dea56731
Konopka Oleg <godschildknopik@gmail.com>
2016-02-01 15:36:41 +0200

    Add a new column to a database, add unique index to all the users

--- db/migrate/20160201132314_add_user_name_to_users.rb
+++ db/migrate/20160201132314_add_user_name_to_users.rb
@@ -1,4 +1,6 @@
 class AddUserNameToUsers < ActiveRecord::Migration
-  def change
+  def change # anythink inside  of this method will be translated to SQL code and modified a DB (currently, SQLITE) 
+  	add_column :users, :username, :string #add a new column to table users called username of type "string"
+  	add_index :users, :username, unique: true
   end
 end
