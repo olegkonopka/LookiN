bdf8a78664d93dd185536929a962957814f10d2d
Konopka Oleg <godschildknopik@gmail.com>
2016-02-01 18:49:49 +0200

    Displaying posts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,7 @@ class PagesController < ApplicationController
   end
 #back end code for pages/home
   def home
+     @posts=Post.all
   end
 #back end code for pages/profile
   def profile
@@ -13,8 +14,10 @@ class PagesController < ApplicationController
   	else
   		redirect_to root_path, :notice=> "User not found!!!"
   	end	
+    @posts=Post.all.where("user_id=?", User.find_by_username(params[:id]).id)
   end
 #back end code for pages/explore
   def explore
+     @posts=Post.all
   end
 end
